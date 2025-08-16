13b9b568f1dbef7e63a3e04624189ff89df19399
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-08-16 16:54:16 -0500

    Added header file for person.cpp and assigned their respective responsabilities. Also created generatePerson file

--- person.cpp
+++ person.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include "person.h"
 
 using namespace std;
 using namespace std::chrono;
 
-class Person
+Person::Person(string name, year_month_day birthDate, string currentCity, int id, int assets, int debt, bool isTaxFiler)
 {
+    this->name = name;
+    this->birthDate = birthDate;
+    this->currentCity = currentCity;
+    this->id = id;
+    this->assets = assets;
+    this->debt = debt;
+    this->isTaxFiler = isTaxFiler;
+}
 
-public:
-    // Personal information
-    string name;
-    year_month_day birthDate;
-    string currentCity;
-    int id;
+int Person::getAge()
+{
+    year yearOfBirth = this->birthDate.year();
 
-    // Tax data
-    int assets;      // Total value
-    int debt;        // Due payments
-    bool isTaxFiler; // Declares if the person declares rent.
-    // enum taxCalendarGroup;
+    // system_clock returns a time_point, which must be turned into a year by eliminating the time of day
+    const auto now = floor<days>(system_clock::now());
 
-    Person(string name, year_month_day birthDate, string currentCity, int id, int assets, int debt, bool isTaxFiler)
-    {
-        this->name = name;
-        this->birthDate = birthDate;
-        this->currentCity = currentCity;
-        this->id = id;
-        this->assets = assets;
-        this->debt = debt;
-        this->isTaxFiler = isTaxFiler;
-    }
+    // converts the time_point now into the type year_month_day, and then extracts only the year.
+    year currentYear = year_month_day{floor<days>(now)}.year();
 
-    int getAge()
-    {
-        year yearOfBirth = this->birthDate.year();
+    years timeDifference = currentYear - yearOfBirth;
+    int age = timeDifference.count();
 
-        // system_clock returns a time_point, which must be turned into a year by eliminating the time of day
-        const auto now = floor<days>(system_clock::now());
-
-        // converts the time_point now into the type year_month_day, and then extracts only the year.
-        year currentYear = year_month_day{floor<days>(now)}.year();
-
-        years timeDifference = currentYear - yearOfBirth;
-        int age = timeDifference.count();
-
-        return age;
-    }
-};
+    return age;
+}
---
+++ person.h
@@ -0,0 +1,24 @@
+#ifndef PERSON_INCLUDE
+#define PERSON_INCLUDE
+
+class Person
+{
+
+public:
+    // Personal information
+    std::string name;
+    std::chrono::year_month_day birthDate;
+    std::string currentCity;
+    int id;
+
+    // Tax data
+    int assets;      // Total value
+    int debt;        // Due payments
+    bool isTaxFiler; // Declares if the person declares rent.
+    // enum taxCalendarGroup;
+
+    Person(std::string name, std::chrono::year_month_day birthDate, std::string currentCity, int id, int assets, int debt, bool isTaxFiler);
+    int getAge();
+};
+
+#endif
