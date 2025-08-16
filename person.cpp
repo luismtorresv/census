46a0cabe9e33bf7e4f948d4df9c01e2e6592291d
Jeronimo Acosta <jeronimoacosta7@gmail.com>
2025-08-16 14:18:43 -0500

    Added person.cpp

---
+++ person.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+class Person
+{
+
+public:
+    // Personal information
+    string name;
+    year_month_day birthDate;
+    string currentCity;
+    int id;
+
+    // Tax data
+    int assets;      // Total value
+    int debt;        // Due payments
+    bool isTaxFiler; // Declares if the person declares rent.
+    // enum taxCalendarGroup;
+
+    Person(string name, year_month_day birthDate, string currentCity, int id, int assets, int debt, bool isTaxFiler)
+    {
+        this->name = name;
+        this->birthDate = birthDate;
+        this->currentCity = currentCity;
+        this->id = id;
+        this->assets = assets;
+        this->debt = debt;
+        this->isTaxFiler = isTaxFiler;
+    }
+
+    int getAge()
+    {
+        year yearOfBirth = this->birthDate.year();
+
+        // system_clock returns a time_point, which must be turned into a year by eliminating the time of day
+        const auto now = floor<days>(system_clock::now());
+
+        // converts the time_point now into the type year_month_day, and then extracts only the year.
+        year currentYear = year_month_day{floor<days>(now)}.year();
+
+        years timeDifference = currentYear - yearOfBirth;
+        int age = timeDifference.count();
+
+        return age;
+    }
+};
