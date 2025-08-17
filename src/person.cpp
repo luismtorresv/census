46921a790377ff6a9718b53440c5f4d8f4d50563
Luis Torres <lmtorresv@eafit.edu.co>
2025-08-17 11:33:59 -0500

    Run `make format`

--- src/person.cpp
+++ src/person.cpp
@@ -3,29 +3,30 @@
 using namespace std;
 using namespace std::chrono;
 
-Person::Person(int id, std::string name, std::chrono::year_month_day birthDate, std::string currentCity, int assets, int debt, bool isTaxFiler)
-{
-    this->name = name;
-    this->birthDate = birthDate;
-    this->currentCity = currentCity;
-    this->id = id;
-    this->assets = assets;
-    this->debt = debt;
-    this->isTaxFiler = isTaxFiler;
+Person::Person(int id, std::string name, std::chrono::year_month_day birthDate,
+               std::string currentCity, int assets, int debt, bool isTaxFiler) {
+  this->name = name;
+  this->birthDate = birthDate;
+  this->currentCity = currentCity;
+  this->id = id;
+  this->assets = assets;
+  this->debt = debt;
+  this->isTaxFiler = isTaxFiler;
 }
 
-int Person::getAge()
-{
-    year yearOfBirth = this->birthDate.year();
+int Person::getAge() {
+  year yearOfBirth = this->birthDate.year();
 
-    // system_clock returns a time_point, which must be turned into a year by eliminating the time of day
-    const auto now = floor<days>(system_clock::now());
+  // system_clock returns a time_point, which must be turned into a year by
+  // eliminating the time of day
+  const auto now = floor<days>(system_clock::now());
 
-    // converts the time_point now into the type year_month_day, and then extracts only the year.
-    year currentYear = year_month_day{floor<days>(now)}.year();
+  // converts the time_point now into the type year_month_day, and then extracts
+  // only the year.
+  year currentYear = year_month_day{floor<days>(now)}.year();
 
-    years timeDifference = currentYear - yearOfBirth;
-    int age = timeDifference.count();
+  years timeDifference = currentYear - yearOfBirth;
+  int age = timeDifference.count();
 
-    return age;
+  return age;
 }
--- src/personFactory.cpp
+++ src/personFactory.cpp
@@ -3,89 +3,88 @@
 using namespace std;
 using namespace std::chrono;
 
-Person PersonFactory::createPerson()
-{
-    int id = generateRandomId();
-    string name = generateRandomName();
-    year_month_day birthDate = generateRandomBirthDate();
-    string city = generateRandomCity();
-    int assets = generateRandomAssets();
-    int debt = generateRandomDebt();
-    bool isTaxFiler = generateRandomBoolean();
+Person PersonFactory::createPerson() {
+  int id = generateRandomId();
+  string name = generateRandomName();
+  year_month_day birthDate = generateRandomBirthDate();
+  string city = generateRandomCity();
+  int assets = generateRandomAssets();
+  int debt = generateRandomDebt();
+  bool isTaxFiler = generateRandomBoolean();
 
-    return Person{id, name, birthDate, city, assets, debt, isTaxFiler};
+  return Person{id, name, birthDate, city, assets, debt, isTaxFiler};
 }
 
-int PersonFactory::generateRandomId()
-{
-    static long counter = 1'000'000'000;
-    return counter++;
+int PersonFactory::generateRandomId() {
+  static long counter = 1'000'000'000;
+  return counter++;
 }
 
-string PersonFactory::generateRandomName()
-{
-    const static vector<string> names = {
-        "Juan", "Carlos", "José", "James", "Andrés", "Miguel", "Luis", "Pedro", "Alejandro", "Ricardo",
-        "Felipe", "David", "Jorge", "Santiago", "Daniel", "Edison", "Nestor", "Gertridis", "Aidan", "Solomon",
-        "Glory", "María", "Luisa", "Carmen", "Ana", "Sofía", "Isabel", "Laura", "Andrea", "Paula", "Valentina",
-        "Camila", "Daniela", "Carolina", "Fernanda", "Gabriela", "Patricia", "Claudia", "Caligo", "Diana", "Lucía",
-        "Ximena", "Xenaria"};
+string PersonFactory::generateRandomName() {
+  const static vector<string> names = {
+      "Juan",      "Carlos",   "José",      "James",    "Andrés",   "Miguel",
+      "Luis",      "Pedro",    "Alejandro", "Ricardo",  "Felipe",   "David",
+      "Jorge",     "Santiago", "Daniel",    "Edison",   "Nestor",   "Gertridis",
+      "Aidan",     "Solomon",  "Glory",     "María",    "Luisa",    "Carmen",
+      "Ana",       "Sofía",    "Isabel",    "Laura",    "Andrea",   "Paula",
+      "Valentina", "Camila",   "Daniela",   "Carolina", "Fernanda", "Gabriela",
+      "Patricia",  "Claudia",  "Caligo",    "Diana",    "Lucía",    "Ximena",
+      "Xenaria"};
 
-    const static vector<string> surnames = {
-        "Gómez", "Rodríguez", "Martínez", "López", "García", "Pérez", "González", "Sánchez", "Ramírez", "Torres",
-        "Díaz", "Vargas", "Castro", "Ruiz", "Álvarez", "Romero", "Suárez", "Rojas", "Moreno", "Muñoz", "Valencia",
-        "Griven"};
+  const static vector<string> surnames = {
+      "Gómez",    "Rodríguez", "Martínez", "López",  "García", "Pérez",
+      "González", "Sánchez",   "Ramírez",  "Torres", "Díaz",   "Vargas",
+      "Castro",   "Ruiz",      "Álvarez",  "Romero", "Suárez", "Rojas",
+      "Moreno",   "Muñoz",     "Valencia", "Griven"};
 
-    // Generate a random number to input as position for vectors 'names' and 'surnames'.
+  // Generate a random number to input as position for vectors 'names' and
+  // 'surnames'.
 
-    int random_name_pos = generateRandomNumber(0, names.size());
-    int random_surname_pos = generateRandomNumber(0, surnames.size());
+  int random_name_pos = generateRandomNumber(0, names.size());
+  int random_surname_pos = generateRandomNumber(0, surnames.size());
 
-    return names[random_name_pos] + surnames[random_surname_pos];
+  return names[random_name_pos] + surnames[random_surname_pos];
 }
 
-year_month_day PersonFactory::generateRandomBirthDate()
-{
-    unsigned int rand_day = generateRandomNumber(1, 31);
-    unsigned int rand_month = generateRandomNumber(1, 12);
-    int rand_year = generateRandomNumber(1925, 2025);
+year_month_day PersonFactory::generateRandomBirthDate() {
+  unsigned int rand_day = generateRandomNumber(1, 31);
+  unsigned int rand_month = generateRandomNumber(1, 12);
+  int rand_year = generateRandomNumber(1925, 2025);
 
-    return year{rand_year} / month{rand_month} / day{rand_day}; // this is the expected expression.
+  return year{rand_year} / month{rand_month} /
+         day{rand_day}; // this is the expected expression.
 };
 
-string PersonFactory::generateRandomCity()
-{
-    const static vector<string> colombianCities = {
-        "Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena", "Bucaramanga", "Pereira", "Santa Marta", "Cúcuta", "Ibagué",
-        "Manizales", "Pasto", "Neiva", "Villavicencio", "Armenia", "Sincelejo", "Valledupar", "Montería", "Popayán", "Tunja"};
+string PersonFactory::generateRandomCity() {
+  const static vector<string> colombianCities = {
+      "Bogotá",      "Medellín",   "Cali",        "Barranquilla",  "Cartagena",
+      "Bucaramanga", "Pereira",    "Santa Marta", "Cúcuta",        "Ibagué",
+      "Manizales",   "Pasto",      "Neiva",       "Villavicencio", "Armenia",
+      "Sincelejo",   "Valledupar", "Montería",    "Popayán",       "Tunja"};
 
-    // Generate a random number to input as position for vector 'colombianCities'.
+  // Generate a random number to input as position for vector 'colombianCities'.
 
-    int random_cities_pos = generateRandomNumber(0, colombianCities.size());
+  int random_cities_pos = generateRandomNumber(0, colombianCities.size());
 
-    return colombianCities[random_cities_pos];
+  return colombianCities[random_cities_pos];
 }
 
-int PersonFactory::generateRandomNumber(int min, int max)
-{
-    static mt19937 generator(random_device{}()); // This is the seed
-    uniform_int_distribution<int> distribution(min, max);
-    return distribution(generator);
+int PersonFactory::generateRandomNumber(int min, int max) {
+  static mt19937 generator(random_device{}()); // This is the seed
+  uniform_int_distribution<int> distribution(min, max);
+  return distribution(generator);
 }
 
-int PersonFactory::generateRandomAssets()
-{
-    return generateRandomNumber(1000, 10000);
+int PersonFactory::generateRandomAssets() {
+  return generateRandomNumber(1000, 10000);
 }
 
-int PersonFactory::generateRandomDebt()
-{
-    return generateRandomNumber(1000, 10000);
+int PersonFactory::generateRandomDebt() {
+  return generateRandomNumber(1000, 10000);
 }
 
-bool PersonFactory::generateRandomBoolean()
-{
-    int random_val = generateRandomNumber(0, 1);
+bool PersonFactory::generateRandomBoolean() {
+  int random_val = generateRandomNumber(0, 1);
 
-    return (random_val == 0) ? true : false;
+  return (random_val == 0) ? true : false;
 }
